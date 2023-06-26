c2f807f92d144d92d262a3bca9958cb7a5e9b533
renanlucena <renan.segura.de.lucena@gmail.com>
2023-06-26 15:15:01 -0300

    adicionando a logo do Aluroni

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,11 @@
 import styles from "./Cardapio.module.scss";
+import logo from "../../assets/logo.svg";
 
 export default function Cardapio(){
     return(
         <main>
             <nav className={styles.menu}>
-
+                <img src={logo} alt="Logo do Aluroni"/>
             </nav>
         </main>
     )

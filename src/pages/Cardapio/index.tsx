738056327a1bb883479739a762dd0c0153aee006
renanlucena <renan.segura.de.lucena@gmail.com>
2023-06-26 15:16:40 -0300

    usando logo como um ReactComponent

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,11 +1,11 @@
 import styles from "./Cardapio.module.scss";
-import logo from "../../assets/logo.svg";
+import {ReactComponent as Logo} from "../../assets/logo.svg";
 
 export default function Cardapio(){
     return(
         <main>
             <nav className={styles.menu}>
-                <img src={logo} alt="Logo do Aluroni"/>
+                <Logo />
             </nav>
         </main>
     )

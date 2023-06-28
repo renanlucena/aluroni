5b02d493a31a8e9d7196f65e8da6bd123374d405
renanlucena <renan.segura.de.lucena@gmail.com>
2023-06-28 15:59:40 -0300

    implementando o header

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -3,10 +3,18 @@ import {ReactComponent as Logo} from "assets/logo.svg";
 
 export default function Cardapio(){
     return(
-        <main>
+        <main className={styles.main}>
             <nav className={styles.menu}>
                 <Logo />
             </nav>
+
+                <header className={styles.header}>
+                    <div className={styles.header__text}>
+                    A casa do código e da massa
+                    </div>
+                </header>
+               
+            
         </main>
     )
 }

bc91634f8fe34badc29bbd54361c37a7439a57ac
renanlucena <renan.segura.de.lucena@gmail.com>
2023-07-05 15:06:06 -0300

    fazendo o ordenar funcionar

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./Ordenador.module.scss";
 import opcoes from "./opcoes.json";
+import classNames from "classnames";
+import {MdKeyboardArrowDown, MdKeyboardArrowUp} from "react-icons/md";
 
 export default function Ordenador() {
+    const [aberto, setAberto] = useState(false);
     return (
-        <button className={styles.ordenador}>
+        <button 
+            className={styles.ordenador} 
+            onClick={() => setAberto(!aberto)}
+            onBlur={() => setAberto(false)}
+            >
             <span >Ordenar Por</span>
-            <div className={styles.ordenador__options}>
+            {aberto ? < MdKeyboardArrowUp/> : < MdKeyboardArrowDown/>}
+            <div className={classNames({
+                [styles.ordenador__options]: true,
+                [styles["ordenador__options--ativo"]]: aberto,
+            }) }>
                 {opcoes.map(opcao => (
                     <div className={styles.ordenador__option} key={opcao.value}>
                         {opcao.nome}
